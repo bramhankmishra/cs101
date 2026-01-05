e1997e2ca282e5583e0eaad2cb4d52306a4d9731
bramhankmishra <bramhankmishra12b@gmail.com>
2026-01-05 11:49:11 +0530

    This is day 2 of learning coding (again \!).

---
+++ Warm up/Web3/Warm up class/public key cryptography.js
@@ -0,0 +1,17 @@
+// let newarray = new Uint8Array([1, 225, 123, 2323,221])
+
+// console.log(newarray)
+
+// Write a program to convert BYTES to ASCII
+const prompt = require('prompt-sync')();
+let input_from_user = prompt('Enter the string you want to convert: ')
+let ByteArray = new Uint8Array()
+const output_to_user = ByteArray(input_from_user)
+console.log(output_to_user)
+
+
+// Write a program to convert ASCII to BYTES
+
+// Write a program to convert UNNT8Array to ASCII
+
+// Write a program to convert ASCII to UNNT8Array
